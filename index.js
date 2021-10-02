3029d0cf86eacb1e7b90afc7ef27af4b5e836993
PE2021-1 <pe2021@gmail.com>
2021-10-02 17:45:13 +0300

    add CRUD sql

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { Student } = require('./models');
+const { Student, sequelize } = require('./models');
+const { Op } = require('sequelize');
 
 (async function () {
   const newStudent = {
@@ -11,6 +12,118 @@ const { Student } = require('./models');
     gender: 'male',
   };
 
-  const createdStudent = await Student.create(newStudent, { raw: true });
-  console.log(`createdStudent`, createdStudent);
+  // C - INSERT
+  // const createdStudent = await Student.create(newStudent, { raw: true });
+  // console.log(`createdStudent`, createdStudent);
+
+  // R - SELECT
+  //students with pure data and pagination
+  // const foundStudents = await Student.findAll({
+  //   raw: true,
+  //   limit: 10,
+  //   offset: 0,
+  //   //add ordering
+  //   order: [['discount', 'DESC'], 'firstName'],
+  // });
+
+  //filters
+  // const foundStudents = await Student.findAll({
+  //   raw: true,
+  //   where: {
+  //     discount: 0,
+  //   },
+  //   limit: 5,
+  //   offset: 0,
+  // });
+
+  //some filters with AND
+  // const foundStudents = await Student.findAll({
+  //   raw: true,
+  //   where: {
+  //     discount: 0,
+  //     isPrivatePayed: true,
+  //   },
+  //   limit: 5,
+  //   offset: 0,
+  // });
+
+  //some filters OR
+  // const foundStudents = await Student.findAll({
+  //   raw: true,
+  //   where: {
+  //     [Op.or]: [{ id: 1 }, { id: 2 }],
+  //   },
+  //   limit: 5,
+  //   offset: 0,
+  // });
+
+  // const foundStudents = await Student.findAll({
+  //   raw: true,
+  //   where: {
+  //     firstName: {
+  //       [Op.like]: 'Stud1%',
+  //     },
+  //   },
+  // });
+
+  //proection = select displayed attributes
+  // const foundStudents = await Student.findAll({
+  //   raw: true,
+  //   attributes: ['firstName', 'lastName', 'birthday'],
+  //   limit: 5,
+  //   offset: 0,
+  // });
+
+  //proection with excluded attributes
+  // const foundStudents = await Student.findAll({
+  //   raw: true,
+  //   attributes: { exclude: ['email', 'createdAt', 'updatedAt'] },
+  //   limit: 5,
+  //   offset: 0,
+  // });
+
+  //displayed calculated result
+  // const foundStudents = await Student.findAll({
+  //   raw: true,
+  //   attributes: [
+  //     ['firstName', 'name'],
+  //     'lastName',
+  //     [sequelize.fn('age', sequelize.col('birthday')), 'studAge'],
+  //   ],
+  // });
+
+  //extract some info from function result
+  // const foundStudents = await Student.findAll({
+  //   raw: true,
+  //   attributes: [
+  //     ['firstName', 'name'],
+  //     'lastName',
+  //     sequelize.literal(`EXTRACT (year FROM age(birthday))`),
+  //   ],
+  // });
+
+  //group by
+  // const foundStudents = await Student.findAll({
+  //   raw: true,
+  //   attributes: [
+  //     'gender',
+  //     [sequelize.fn('count', sequelize.col('gender')), 'countGender'],
+  //   ],
+  //   group: 'gender',
+  // });
+
+  //console.log(`foundStudents`, foundStudents);
+
+  //U - update
+  // const updatedStudents = await Student.update(
+  //   { firstName: 'Irina' },
+  //   { where: { id: 5 }, returning: true, raw: true }
+  // );
+  // console.log(`updatedStudents`, updatedStudents);
+
+  //D - delete
+  // const deletedStudents = await Student.destroy({
+  //   where: { id: 95 },
+  // });
+  // console.log(`deletedStudents`, deletedStudents);
 })();
---
+++ seeders/20211002123727-Student.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const studDbData = [];
+
+for (let i = 0; i <= 100; i++) {
+  studDbData.push({
+    firstName: `Stud${i}`,
+    lastName: `SurnameStud${i}`,
+    email: `stud${i}@gmail.com`,
+    birthday: new Date(
+      2000 + Math.trunc(Math.random() * 10),
+      Math.trunc(Math.random() * 11),
+      Math.trunc(Math.random() * 28)
+    ),
+    isPrivatePayed: Math.random() > 0.25 ? false : true,
+    discount: Math.trunc(Math.random() * 5),
+    gender: Math.random() > 0.5 ? 'male' : 'female',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Students', studDbData, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Students', null, {});
+  },
+};
